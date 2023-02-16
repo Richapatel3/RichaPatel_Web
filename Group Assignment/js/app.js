de497d4f95af828589c784d0b876025923ce6adc
Richa Patel <patel.richa1@northeastern.edu>
2023-02-16 14:07:28 -0500

    Thrift Website - Bootstrap Components

---
+++ Group Assignment/js/app.js
@@ -0,0 +1,77 @@
+/* Main JS file of the templet.
+for the home page 
+*/
+
+function Scrollbar() {
+  const navbar = document.getElementById("navbar");
+  if (
+    document.body.scrollTop >= 50 ||
+    document.documentElement.scrollTop >= 50
+  ) {
+    navbar.classList.add("nav-sticky");
+  } else {
+    navbar.classList.remove("nav-sticky");
+  }
+}
+window.addEventListener("scroll", (ev) => {
+  ev.preventDefault();
+  Scrollbar();
+});
+
+
+var mainbutton = document.getElementById("back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    mainbutton.style.display = "block";
+  } else {
+    mainbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+//
+/* Switcher js */
+//
+
+function toggleSwitcher() {
+  var i = document.getElementById("style-switcher");
+  if (i.style.left === "-189px") {
+    i.style.left = "-0px";
+  } else {
+    i.style.left = "-189px";
+  }
+}
+
+function setColor(theme) {
+  document.getElementById("color-opt").href = "./css/colors/" + theme + ".css";
+  toggleSwitcher(false);
+}
+
+//
+/*Page Load js */
+//
+
+window.onload = function loader() {
+  setTimeout(() => {
+    document.getElementById("preloader").style.visibility = "hidden";
+    document.getElementById("preloader").style.opacity = "0";
+  }, 300);
+};
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
